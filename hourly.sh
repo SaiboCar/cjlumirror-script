20bc8d193aa111fe18e42316edf32771ce79dea1
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-04-03 12:02:14 +0000

    up

--- hourly.sh
+++ hourly.sh
@@ -13,6 +13,6 @@ mkdir -p /var/repos/ubuntu
 docker run --rm \
     -e APTSYNC_URL='http://mirrors.ustc.edu.cn/ubuntu/' \
     -e APTSYNC_UNLINK=1 \
-    -e APTSYNC_DISTS='bionic focal|main multiverse restricted universe|amd64|/data' \
+    -e APTSYNC_DISTS='bionic focal|main multiverse restricted universe|amd64' \
     -v /var/repos/ubuntu:/data \
-    ustcmirror/apt-sync:latest &
+    ustcmirror/aptsync:latest &

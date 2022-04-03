508e85d43e630fe97ae9f6d83472301529bc0ea8
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-04-03 13:27:34 +0000

    up

--- hourly.sh
+++ hourly.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/sudo /bin/sh
 mkdir -p /var/sync-logs/alpine /var/repos/alpine
-docker run --rm \
+docker stop syncalpine
+docker run --name syncalpine --rm \
     -e LOG_ROTATE_CYCLE='5' \
     -e RSYNC_HOST='rsync.mirrors.ustc.edu.cn' \
     -e RSYNC_PATH='alpine/' \
@@ -10,7 +11,10 @@ docker run --rm \
     ustcmirror/rsync:latest &
 
 mkdir -p /var/repos/ubuntu
-docker run --rm \
+rm -fr /var/repos/ubuntu/ubuntu
+ln -s .. /var/repos/ubuntu/ubuntu
+docker stop syncubuntu
+docker run --name syncubuntu --rm \
     -e APTSYNC_URL='http://mirrors.ustc.edu.cn/ubuntu/' \
     -e APTSYNC_UNLINK=1 \
     -e APTSYNC_DISTS='bionic focal|main multiverse restricted universe|amd64' \

03fe2a49cf81686b8b4ac4f9f21be3bc053ee960
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-04-03 13:31:14 +0000

    up

--- hourly.sh
+++ hourly.sh
@@ -8,14 +8,14 @@ docker run --name syncalpine --rm \
     -e RSYNC_MAXDELETE='10000' \
     -v /var/repos/alpine:/data \
     -v /var/sync-logs/alpine:/log \
-    ustcmirror/rsync:latest 'until /sync.sh; do :;done'&
+    ustcmirror/rsync:latest 'until /sync.sh; do :;done' &
 
 mkdir -p /var/repos/ubuntu
 rm -fr /var/repos/ubuntu/ubuntu
 ln -s .. /var/repos/ubuntu/ubuntu
 docker stop syncubuntu
 docker run --name syncubuntu --rm \
-    -e APTSYNC_URL='http://mirrors.ustc.edu.cn/ubuntu/' \
+    -e APTSYNC_URL='http://mirrors.zju.edu.cn/ubuntu/' \
     -e APTSYNC_UNLINK=1 \
     -e APTSYNC_DISTS='bionic focal|main multiverse restricted universe|amd64' \
     -e TO='/data' \
